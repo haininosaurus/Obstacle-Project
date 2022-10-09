f3ad0880fc4bba5ef8f00ca75e95b54e61e2c0eb
DESKTOP-P9DS2L4\ADMIN <lamsonhai123321@gmail.com>
2022-10-09 19:31:06 +0700

    [UPDATE] init project

---
+++ Assets/Mover.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
